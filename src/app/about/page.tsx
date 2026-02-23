335ca5391a0538d71542be3d46ab90a0452c30c2
WahajHassan883 <wahajhassan007@gmail.com>
2026-02-23 08:10:34 +0500

    feat: add initial About page with a hero section and associated image.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import AboutHeroSection from "@/components/about/about-hero-section";
+
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <AboutHeroSection />
+    </main>
+  );
+}
---
+++ src/components/about/about-hero-section.tsx
@@ -0,0 +1,7 @@
+export default function AboutHeroSection() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
