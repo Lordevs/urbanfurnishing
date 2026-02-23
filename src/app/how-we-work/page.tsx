39c1b6235cb9685d9aded0466687af0b754e352b
WahajHassan883 <wahajhassan007@gmail.com>
2026-02-23 09:04:50 +0500

    feat: Add new 'How We Work' page and its hero component.

---
+++ src/app/how-we-work/page.tsx
@@ -0,0 +1,9 @@
+import HowWeWorkHero from "@/components/how-we-work/how-we-work-hero";
+
+export default function HowWeWork() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <HowWeWorkHero />
+    </main>
+  );
+}
---
+++ src/components/how-we-work/how-we-work-hero.tsx
@@ -0,0 +1,3 @@
+export default function HowWeWorkHero() {
+  return <section></section>;
+}
