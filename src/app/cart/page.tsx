10085c745ed147bab9cef1b15d102ede893aad79
WahajHassan883 <wahajhassan007@gmail.com>
2026-03-11 05:22:58 +0500

    feat: add cart page with a new hero section component.

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,9 @@
+import CartHeroSection from "@/components/cart/card-hero-section";
+
+export default function CartPage() {
+  return (
+    <div className="min-h-screen">
+      <CartHeroSection />
+    </div>
+  );
+}
